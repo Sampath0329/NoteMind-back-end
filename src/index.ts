0ee89723adcd29f73ad12bda8546fca7ebd22150
Sampath <sampathnadeesh0329@gmail.com>
2026-01-10 03:47:29 +0530

    Update configurations

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "https://note-mind-front-end.vercel.app",
-  "https://rad-final-note-mind-front-end.vercel.app/"
+  "https://rad-final-note-mind-front-end.vercel.app"
 ];
 
 app.use(cors({
